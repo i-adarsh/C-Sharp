09002716d14f92cb9bc58c3a91a0908b52633a93
i-adarsh <i.adarsh.kr@icloud.com>
2021-05-20 00:11:06 +0530

    Latest Update

---
+++ fullName.cs
@@ -0,0 +1,12 @@
+using System;
+namespace Sonu{
+    public class Adarsh{
+        public static void Main(){
+            Console.Write("Enter Your First Name : ");
+            string n1 = Console.ReadLine();
+            Console.Write("Enter Your Last Name : ");
+            string n2 = Console.ReadLine();
+            Console.Write("Your full name is {0} {1}\n", n1, n2);
+        }
+    }
+}

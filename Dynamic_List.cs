28879f5aeaa2e32f4040f20c8f3419a11052b13e
i-adarsh <i.adarsh.kr@icloud.com>
2021-05-20 13:51:03 +0530

    Latest Update

--- Dynamic_List.cs
+++ Dynamic_List.cs
@@ -1,18 +1,19 @@
 using System;
+using System.Collections.Generic;
 
 namespace Sonu{
     public class Adarsh{
         public static void Main(){
-            List<Integer> marks = new List<Integer>();
+            List<int> marks = new List<int>();
             char choice = 'y';
             while(choice == 'y'){
                 Console.Write("Enter Your Number : ");
                 int a = Convert.ToInt32(Console.ReadLine());
-                marks.add(a);
+                marks.Add(a);
                 Console.Write("Do you want to continue (y/n):");
                 choice = Console.ReadLine()[0];
             }
-            for (int i = 0; i < marks.sise(); i++){
+            for (int i = 0; i < marks.Count; i++){
                 Console.Write("{0} ", marks[i]);
             }
         }

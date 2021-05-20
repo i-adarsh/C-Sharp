8e82be17d409f42d106e614449e5650fae970d6c
i-adarsh <i.adarsh.kr@icloud.com>
2021-05-20 23:25:46 +0530

    Latest Update

--- test2.cs
+++ test2.cs
@@ -1,9 +1,65 @@
 using System;
 
-namespace Sonu{
-    public class Adarsh{
-        public static void Main(){
-            
-        }
-    }
+class sample
+
+{
+
+public int i;
+
+public int[] arr = new int[10];
+
+public void fun(int i, int val)
+
+{
+
+arr[i] = val;
+
 }
+
+}
+
+class Program
+
+{
+
+static void Main(string[] args)
+
+{
+
+sample s = new sample();
+
+s.i = 10;
+
+sample.fun(1, 5);
+
+s.fun(1, 5);
+
+Console.ReadLine();
+
+}
+
+}
+
+// namespace Sonu{
+//     public class Adarsh{
+//         public static void Main(){
+//             int a=5,s=10;
+
+// if(a<s ||s<0)
+
+//  Console.Write("Red");
+
+// else
+
+//  Console.Write("Blue");
+//         }
+//     }
+// }
+
+
+
+// string[] str = {"Hello", "!", "World"};
+
+// Array.Reverse(str);
+
+// Console.WriteLine(str[0]);

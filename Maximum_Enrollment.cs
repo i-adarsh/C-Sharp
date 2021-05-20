6240234467acd0f08a2f19d821872723e743f03b
i-adarsh <i.adarsh.kr@icloud.com>
2021-05-20 14:25:40 +0530

    Latest Update

--- Maximum_Enrollment.cs
+++ Maximum_Enrollment.cs
@@ -4,7 +4,23 @@ using System.Collections.Generic;
 namespace Sonu{
     public class Adarsh{
         public static void Main(){
-            
+            Console.Write("Total students placed in CS : ");
+            int cs = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter students placed in MECH : ");
+            int mech = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter students placed in MET : ");
+            int met = Convert.ToInt32(Console.ReadLine());
+
+            int max = Math.max(Math.max(cs, mech), met);
+            if (max == cs){
+                Console.WriteLine("Highest placement CS");
+            }
+            if (max == mech){
+                Console.WriteLine("Highest placement MECH")
+            }
+            if (max == met){
+                Console.WriteLine("Highest placement MET");
+            }
         }
     }
 }

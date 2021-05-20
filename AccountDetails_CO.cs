2eac2f17790463f5888c0a1650c3209bb729b298
i-adarsh <i.adarsh.kr@icloud.com>
2021-05-20 22:21:45 +0530

    Latest Update

--- AccountDetails_CO.cs
+++ AccountDetails_CO.cs
@@ -22,6 +22,7 @@ public class Account{
                 return balance;
             }
         }
+        set{balance = value;}
     }
     public bool WithDraw (double amount){
         if (balance > amount){

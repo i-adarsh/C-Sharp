dea6fece2bc95550cba4bcc41d334e0a0fbc2c09
i-adarsh <i.adarsh.kr@icloud.com>
2021-05-20 21:04:24 +0530

    Latest Update

---
+++ Find the Age of a person.cs
@@ -0,0 +1,62 @@
+using System;
+
+public class Person{
+    private String firstName;
+    private String lastName;
+    private DateTime dob;
+
+    public String FirstName{
+        set{firstName = value;}
+        get { return firstName;}
+    }
+    public String LastName{
+        set{ lastName = value;}
+        get { return lastName;}
+    }
+    public DateTime Dob{
+        set{dob = value;}
+        get { return dob;}
+    }
+    public int GetAge(DateTime dob){
+        int age = 0;
+        age = DateTime.Now.Year - dob.Year;
+        if (DateTime.Now.DayOfYear < dob.DayOfYear){
+            age = age - 1;
+            return age;
+        }
+        return age;
+    }
+    public String Adult(){
+        int age = GetAge(dob);
+        if (age > 18){
+            return "Adult";
+            }
+            else{
+            return "Child";
+            }
+    }
+
+    public void DisplayDetails(){
+        Console.WriteLine("First Name: {0}", firstName);
+        Console.WriteLine("Last Name: {0}", lastName);
+        Console.WriteLine("Age: {0}", GetAge(dob));
+        Console.WriteLine(Adult());
+    }
+    
+}
+
+public class Program{
+    public static void Main(){
+        Console.WriteLine("Enter first name");
+        string firstName = Console.ReadLine();
+        Console.WriteLine("Enter last name");
+        string lastName = Console.ReadLine();
+        Console.WriteLine("Enter date of birth in yyyy/mm/dd/ format");
+        DateTime dob = Convert.ToDateTime(Console.ReadLine());
+        Person person = new Person();
+        person.FirstName = firstName;
+        person.LastName = lastName;
+        person.Dob = dob;
+        person.DisplayDetails();
+    }
+}

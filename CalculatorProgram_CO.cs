45f1d25542d191cbb9c48e0147ee04aabee28182
i-adarsh <i.adarsh.kr@icloud.com>
2021-05-20 22:50:18 +0530

    Latest Update

--- CalculatorProgram_CO.cs
+++ CalculatorProgram_CO.cs
@@ -17,24 +17,25 @@ public class CalculatorProgram{
 }
 
 public class Program{
-    public static void Main(System [] args){
+    public static void Main(){
         Console.WriteLine("Enter the operator");
         char choice = Console.ReadLine()[0];
-        Console.WriteLine("Enter the operands")
+        Console.WriteLine("Enter the operands");
         int a = Convert.ToInt32(Console.ReadLine());
         int b = Convert.ToInt32(Console.ReadLine());
         CalculatorProgram calculate = new CalculatorProgram();
         if (choice == '+'){
-            Console.WriteLine("Result of {0} {1} {2} is {3}", a, choice, b, calculate.Addition(a,b););
+            Console.WriteLine("Result of {0} {1} {2} is {3}", a, choice, b, calculate.Addition(a,b));
         }
         else if (choice == '*'){
-            Console.WriteLine("Result of {0} {1} {2} is {3}", a, choice, b, calculate.Multiplication(a,b););
+            Console.WriteLine("Result of {0} {1} {2} is {3}", a, choice, b, calculate.Multiplication(a,b));
         }
         else if (choice == '-'){
-            Console.WriteLine("Result of {0} {1} {2} is {3}", a, choice, b, calculate.Substraction(a,b););
+            Console.WriteLine("Result of {0} {1} {2} is {3}", a, choice, b, calculate.Substraction(a,b));
         }
         else if (choice == '/'){
-            Console.WriteLine("Result of {0} {1} {2} is {3}", a, choice, b, calculate.Division(a,b););
+            Console.WriteLine("Result of {0} {1} {2} is {3}", a, choice, b, calculate.Division(a,b, out double remainder));
+            Console.WriteLine($"Remainder ={remainder}");
         }
         else{
             Console.WriteLine("Invalid Operator");

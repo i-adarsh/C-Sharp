59950cb5c06ac4c428aadd87a82f2503d1a234f3
i-adarsh <i.adarsh.kr@icloud.com>
2021-05-20 22:42:26 +0530

    Latest Update

--- CalculatorProgram_CO.cs
+++ CalculatorProgram_CO.cs
@@ -1,2 +1,43 @@
 using System;
 
+public class CalculatorProgram{
+    public int Addition (int a, int b){
+        return (a + b);
+    }
+    public int Substraction (int a, int b){
+        return (a - b);
+    }
+    public int Multiplication (int a, int b){
+        return (a * b);
+    }
+    public double Division (int a, int b, out double remainder){
+        remainder = a % b;
+        return (a / b);
+    }
+}
+
+public class Program{
+    public static void Main(System [] args){
+        Console.WriteLine("Enter the operator");
+        char choice = Console.ReadLine()[0];
+        Console.WriteLine("Enter the operands")
+        int a = Convert.ToInt32(Console.ReadLine());
+        int b = Convert.ToInt32(Console.ReadLine());
+        CalculatorProgram calculate = new CalculatorProgram();
+        if (choice == '+'){
+            Console.WriteLine("Result of {0} {1} {2} is {3}", a, choice, b, calculate.Addition(a,b););
+        }
+        else if (choice == '*'){
+            Console.WriteLine("Result of {0} {1} {2} is {3}", a, choice, b, calculate.Multiplication(a,b););
+        }
+        else if (choice == '-'){
+            Console.WriteLine("Result of {0} {1} {2} is {3}", a, choice, b, calculate.Substraction(a,b););
+        }
+        else if (choice == '/'){
+            Console.WriteLine("Result of {0} {1} {2} is {3}", a, choice, b, calculate.Division(a,b););
+        }
+        else{
+            Console.WriteLine("Invalid Operator");
+        }
+    }
+}

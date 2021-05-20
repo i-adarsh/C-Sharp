031ea2c83b7f02b51c10c7c8b5d54f06106f0364
i-adarsh <i.adarsh.kr@icloud.com>
2021-05-20 13:56:04 +0530

    Latest Update

--- Dynamic_Member_Registration.cs
+++ Dynamic_Member_Registration.cs
@@ -1,2 +1,12 @@
 using System;
 using System.Collections.Generic;
+
+namespace Sonu{
+    public class Adarsh{
+        public static void Main(){
+            List<string> names = new List<string>();
+            List<string> places = new List<string>();
+            List<char> status = new List<char>();
+        }
+    }
+}

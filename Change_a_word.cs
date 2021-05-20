3a99d977944764c94da6c21760e3d189bdc8d36a
i-adarsh <i.adarsh.kr@icloud.com>
2021-05-21 00:04:52 +0530

    Latest Update

--- Change_a_word.cs
+++ Change_a_word.cs
@@ -5,12 +5,19 @@ namespace Sonu{
         public static void Main(){
             Console.WriteLine("Enter a string");
             string msg = Console.ReadLine();
-            string str = (string[])msg.Split(' ');
-            if (str.Contains('the')){
-                Console.WriteLine("True");
+            string [] str = (string[])msg.Split(' ');
+            if (msg.Contains("the")){
+                for (int i = 0; i < str.Length; i++){
+                    if(str[i] == "the"){
+                        Console.Write("that ");
+                    }
+                    else{
+                        Console.Write(str[i] + " ");
+                    }
+                }
             }
             else{
-                Console.WriteLine("False");
+                Console.WriteLine("Word 'the' not found");
             }
         }
     }

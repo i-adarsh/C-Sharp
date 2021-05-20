b0fcd2bcaab522f7ea86487ec7b3a943b997aa2e
i-adarsh <i.adarsh.kr@icloud.com>
2021-05-20 14:03:06 +0530

    Latest Update

--- Dynamic_Member_Registration.cs
+++ Dynamic_Member_Registration.cs
@@ -7,6 +7,26 @@ namespace Sonu{
             List<string> names = new List<string>();
             List<string> places = new List<string>();
             List<char> status = new List<char>();
+
+            char choice = 'y';
+            while(choice == 'y'){
+                Console.Write("Enter Name : ");
+                names.Add(Console.ReadLine());
+                Console.Write("Enter Place : ");
+                places.Add(Console.ReadLine());
+                Console.Write("Enter marital status(y/n) : ");
+                status.Add(Console.ReadLine()[0]);
+                Console.Write("Do you wish to continue(y/n) : ");
+                choice = Console.ReadLine()[0];
+                Console.WriteLine();
+            }
+            for (int i = 0; i <names.Count; i++){
+                Console.WriteLine("Confirming Information");
+                Console.WriteLine("{0}", names[i]);
+                Console.WriteLine("{0}", places[i]);
+                Console.WriteLine("Married : {0}", status[i]);
+                Console.WriteLine();
+            }
         }
     }
 }

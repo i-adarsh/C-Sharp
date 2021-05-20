4abb132ccb3a2bb05711b27a475a8c4c999f7556
i-adarsh <i.adarsh.kr@icloud.com>
2021-05-20 17:07:16 +0530

    Latest Update

--- Test_Your_Logic.cs
+++ Test_Your_Logic.cs
@@ -3,7 +3,12 @@ using System;
 namespace Sonu{
     public class Adarsh{
         public static void Main(){
-            Console.WriteLine("//\\");
+            Console.Write("Number of Symbols : ");
+            int symbols = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < symbols; i++){
+                Console.Write("/\\");
+            }
+            
         }
     }
 }

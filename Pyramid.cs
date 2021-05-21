136dce333e4fa2c1b814dfef9e937fbf6e8303c1
i-adarsh <i.adarsh.kr@icloud.com>
2021-05-21 11:41:03 +0530

    Latest Update

---
+++ Pyramid.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Adarsh{
+    public static void Main(){
+        Console.WriteLine("Enter number of repeats :");
+        int rows = Convert.ToInt32(Console.ReadLine());
+        int k = 0;
+        for (int i = 1; i <= rows; ++i, k = 0) {
+            for (int space = 1; space <= rows - i; ++space) {
+                Console.Write("  ");
+            }
+
+        while (k != 2 * i - 1) {
+        Console.Write("* ");
+        ++k;
+        }
+        Console.WriteLine();
+    }
+    }
+}

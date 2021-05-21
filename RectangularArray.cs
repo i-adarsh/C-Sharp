ad2c9429cea774d516839c2d74244cd7fa0fd3ef
i-adarsh <i.adarsh.kr@icloud.com>
2021-05-21 11:19:04 +0530

    Latest Update

--- RectangularArray.cs
+++ RectangularArray.cs
@@ -1,2 +1,32 @@
 using System;
 
+public class Program{
+    static void Main(string[] args){
+        Console.WriteLine("Enter a number:");
+        int num = Convert.ToInt32(Console.ReadLine());
+        int [,] matrix = GetArray(num);
+        for (int i = 0; i < num; i++){
+            for (int j = 0; j < num; j++){
+                Console.Write(matrix[i,j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+    public static int[,] GetArray(int num){
+        int [,] matrix = new int [num, num];
+        for (int i = 0; i < num; i++){
+            for (int j = 0; j < num; j++){
+                if (i == j){
+                    matrix[i,j] = 0;
+                }
+                else if(i < j){
+                    matrix[i,j] = 1;
+                }
+                else{
+                    matrix[i,j] = -1;
+                }
+            }
+        }
+        return matrix;
+    }
+}
